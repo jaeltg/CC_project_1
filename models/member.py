ebc180d9e7a4e31327c8a4921a97ec1ea70e2d63
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:48:42 +0000

    created actual member model

--- models/member.py
+++ models/member.py
@@ -1,3 +1,11 @@
 class Member:
 
-    
+    def __init__(self, name, date_of_birth, memb_number, memb_type, address, contact_number, active = True, id = None):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.memb_number = memb_number
+        self.memb_type = memb_type
+        self.address = address
+        self.contact_number = contact_number
+        self.active = active
+        self.id = id 
