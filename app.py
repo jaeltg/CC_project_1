320336cd37d3eba6d1fd8c8b0256d6682fae929a
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:22:11 +0000

    changed names  from class to yogaclass to avoid python errors due to keyword

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 from controllers.bookings_controller import bookings_blueprint
-from controllers.classes_controller import classes_blueprint
+from controllers.yogaclasses_controller import yogaclasses_blueprint
 from controllers.members_controller import members_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(bookings_blueprint)
-app.register_blueprint(classes_blueprint)
+app.register_blueprint(yogaclasses_blueprint)
 app.register_blueprint(members_blueprint)
 
 @app.route('/')
