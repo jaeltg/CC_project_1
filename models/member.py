36ba60bffbe5a6bbf4bbdf872bca1e0ce9f82f00
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:45:54 +0000

    created yogaclass model

--- models/member.py
+++ models/member.py
@@ -1,10 +1,3 @@
 class Member:
 
-    def __init__(self, name, duration, instructor, time, capacity, active = True, id = None):
-        self.name = name
-        self.duration = duration
-        self.instructor = instructor
-        self.time = time
-        self.capacity = capacity
-        self.active = active
-        self.id = id
+    
---
+++ models/yogaclass.py
@@ -0,0 +1,10 @@
+class YogaClass:
+
+   def __init__(self, name, duration, instructor, time, capacity, active = True, id = None):
+        self.name = name
+        self.duration = duration
+        self.instructor = instructor
+        self.time = time
+        self.capacity = capacity
+        self.active = active
+        self.id = id
