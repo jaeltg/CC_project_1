ddc39dcfe2d9493804bd9c1ba3e6a9622079f255
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:15:32 +0000

    added console.py imports

---
+++ console.py
@@ -0,0 +1,8 @@
+import pdb
+from models.class import Class
+from models.member import Member
+from models.booking import Booking
+
+import repositories.class_repository as class_repository
+import repositories.member_repository as member_repository
+import repositories.booking_repository as booking_repository
