10729f46d1c94c82dafed175b709067545914422
jaeltg <jaeltg@hotmail.com>
2021-03-21 15:16:41 +0000

    checked functionality of save function in console.py

--- console.py
+++ console.py
@@ -6,3 +6,10 @@ from models.booking import Booking
 import repositories.yogaclass_repository as yogaclass_repository
 import repositories.member_repository as member_repository
 import repositories.booking_repository as booking_repository
+
+member1 = Member("Jessica Day", "13/02/1982", 6540003, "Standard", "The Loft, LA", "07911 123444")
+member_repository.save(member1)
+
+
+
+pdb.set_trace()
