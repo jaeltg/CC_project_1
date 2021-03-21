c20c47f4952f591ecfea450c2880edb8929cded4
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:31:38 +0000

    added bookings controller imports

---
+++ controllers/bookings_controller.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.booking import Booking
+import repositories.booking_repository as booking_repository
+import repositories.member_repository as member_repository
+import repositories.yogaclass_repository as yogaclass_repository
+
+bookings_blueprint = Blueprint("bookings", __name__)
