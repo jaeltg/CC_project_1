06bdc1de316eb6813cadd4fa40e3ebf6415974d2
jaeltg <jaeltg@hotmail.com>
2021-03-21 14:11:02 +0000

    created database tables

---
+++ db/yoga_studio.sql
@@ -0,0 +1,31 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE yogaclasses;
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth VARCHAR(255),
+    memb_number INT,
+    memb_type VARCHAR(255),
+    address VARCHAR(500),
+    contact_number VARCHAR(255),
+    active BOOLEAN
+);
+
+CREATE TABLE yogaclasses (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    duration INT,
+    description TEXT,
+    instructor VARCHAR(255),
+    time VARCHAR(255),
+    capacity INT,
+    active BOOLEAN
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    yogaclass_id INT REFERENCES yogaclasses(id) ON DELETE CASCADE,
+);
