96462b64ef33def1af55315310d2e4cff64af05e
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:20:36 +0000

    changed names  from class to yogaclass to avoid python errors due to keyword

--- console.py
+++ console.py
@@ -1,8 +1,8 @@
 import pdb
-from models.yogaclass import Class
+from models.yogaclass import YogaClass
 from models.member import Member
 from models.booking import Booking
 
-import repositories.class_repository as class_repository
+import repositories.yogaclass_repository as yogaclass_repository
 import repositories.member_repository as member_repository
 import repositories.booking_repository as booking_repository
