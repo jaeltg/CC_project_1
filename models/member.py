609e2b0ed142aeab2be985e6ee0aad7d696ae87f
jaeltg <jaeltg@hotmail.com>
2021-03-21 13:44:34 +0000

    created member model

---
+++ models/member.py
@@ -0,0 +1,10 @@
+class Member:
+
+    def __init__(self, name, duration, instructor, time, capacity, active = True, id = None):
+        self.name = name
+        self.duration = duration
+        self.instructor = instructor
+        self.time = time
+        self.capacity = capacity
+        self.active = active
+        self.id = id
