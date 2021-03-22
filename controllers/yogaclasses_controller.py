97bb2f58c93bfae2124211bd9438a91af6d70dd6
jaeltg <jaeltg@hotmail.com>
2021-03-22 14:56:28 +0000

    added route and html tenplate for showing one class

--- controllers/yogaclasses_controller.py
+++ controllers/yogaclasses_controller.py
@@ -8,4 +8,9 @@ yogaclasses_blueprint = Blueprint("yogaclasses", __name__)
 @yogaclasses_blueprint.route('/yogaclasses')
 def yogaclass():
     yogaclasses = yogaclass_repository.select_all()
-    return render_template("yogaclasses/index.html", title = "Classes", all_yogaclasses=yogaclasses)    
+    return render_template("yogaclasses/index.html", title = "Classes", all_yogaclasses=yogaclasses)
+
+@yogaclasses_blueprint.route('/yogaclasses/<id>')
+def show_yogaclass(id):
+    yogaclass = yogaclass_repository.select(id)
+    return render_template("yogaclasses/show.html", title=yogaclass.name, selected_yogaclass = yogaclass)
