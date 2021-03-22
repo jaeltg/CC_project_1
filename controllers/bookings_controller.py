a6417a2293a12219ac0147a416e88b1e54a16dbc
jaeltg <jaeltg@hotmail.com>
2021-03-22 15:55:30 +0000

    added route and form for making a booking

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -6,3 +6,11 @@ import repositories.member_repository as member_repository
 import repositories.yogaclass_repository as yogaclass_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
+
+
+
+@bookings_blueprint.route("/bookings/new", methods=['GET'])
+def new_booking():
+    members = member_repository.select_all()
+    yogaclasses = yogaclass_repository.select_all()
+    return render_template("bookings/new.html", all_members = members, all_yogaclasses = yogaclasses)
