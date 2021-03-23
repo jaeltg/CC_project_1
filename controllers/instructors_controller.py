edc36f78a7bc1b9c72dac9ff8fcc508e87d27151
jaeltg <jaeltg@hotmail.com>
2021-03-23 11:26:35 +0000

    added instructor index template

--- controllers/instructors_controller.py
+++ controllers/instructors_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
-from models.instructors import Instructor
+from models.instructor import Instructor
 import repositories.instructor_repository as instructor_repository
 
 instructors_blueprint = Blueprint("instructors", __name__)
