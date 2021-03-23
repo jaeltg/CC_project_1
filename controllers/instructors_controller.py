e6bdef3d6f737f558a2d373b76cdf5c9a0f02283
jaeltg <jaeltg@hotmail.com>
2021-03-23 11:16:56 +0000

    added instructor controllers

--- controllers/instructors_controller.py
+++ controllers/instructors_controller.py
@@ -5,3 +5,42 @@ import repositories.instructor_repository as instructor_repository
 
 instructors_blueprint = Blueprint("instructors", __name__)
 
+@instructors_blueprint.route('/instructors')
+def instructors():
+    instructors = instructor_repository.select_all()
+    return render_template("instructors/index.html", title = "Instructors", all_instructors=instructors)    
+
+@instructors_blueprint.route('/instructors/new')
+def new_instructor():
+    return render_template("instructors/new.html", title="Add Instructor")
+
+@instructors_blueprint.route('/instructors', methods=['POST'])
+def create_instructor():
+    name = request.form["name"]
+    contact_number = request.form["contact_number"]
+
+    instructor = Instructor(name, contact_number)
+
+    instructor_repository.save(instructor)
+
+    return redirect('/instructors')
+
+@instructors_blueprint.route('/instructors/<id>/edit', methods =['GET'])
+def edit_instructor(id):
+    instructor = instructor_repository.select(id)
+    return render_template('instructors/edit.html', title = "Edit Instructor", selected_instructor = instructor)
+
+@instructors_blueprint.route("/instructors/<id>", methods=['POST'])
+def update_instructor(id):
+    name = request.form["name"]
+    contact_number = request.form["contact_number"]
+
+    instructor = Instructor(name, contact_number, id)
+
+    instructor_repository.update(instructor)
+    return redirect('/instructors')
+
+@instructors_blueprint.route("/instructors/<id>/delete", methods=['POST'])
+def delete_instructor(id):
+    instructor_repository.delete(id)
+    return redirect('/instructors')
