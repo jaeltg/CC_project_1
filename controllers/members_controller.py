297eb1d1448acd960c3341e792cdb5a473ef73cb
jaeltg <jaeltg@hotmail.com>
2021-03-23 12:57:12 +0000

    refactored member controller

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+
 from models.member import Member
+
 import repositories.member_repository as member_repository
+import repositories.memb_type_repository as memb_type_repository
 
 members_blueprint = Blueprint("members", __name__)
 
@@ -18,18 +21,22 @@ def show_member(id):
 
 @members_blueprint.route('/members/new')
 def new_member():
-    return render_template("members/new.html", title="Add Member")
+    memb_types = memb_type_repository.select_all()
+    return render_template("members/new.html", title="Add Member", all_memb_types=memb_types)
 
 @members_blueprint.route('/members', methods=['POST'])
 def create_member():
+    image_url = request.form["image_url"]
     name = request.form["name"]
     date_of_birth = request.form["date_of_birth"]
     memb_number = request.form["memb_number"]
-    memb_type = request.form["memb_type"]
+    memb_type_id = request.form["memb_type"]
     address = request.form["address"]
     contact_number = request.form["contact_number"]
 
-    member = Member(name, date_of_birth, memb_number, memb_type, address, contact_number)
+    memb_type = memb_type_repository.select(memb_type_id)
+
+    member = Member(image_url, name, date_of_birth, memb_number, memb_type, address, contact_number)
 
     member_repository.save(member)
 
@@ -38,19 +45,23 @@ def create_member():
 @members_blueprint.route('/members/<id>/edit', methods =['GET'])
 def edit_member(id):
     member = member_repository.select(id)
-    return render_template('members/edit.html', title = "Edit Member", selected_member = member)
+    memb_types = memb_type_repository.select_all()
+    return render_template('members/edit.html', title = "Edit Member", selected_member = member, all_memb_types = memb_types)
 
 @members_blueprint.route("/members/<id>", methods=['POST'])
 def update_member(id):
+    image_url = request.form["image_url"]
     name = request.form["name"]
     date_of_birth = request.form["date_of_birth"]
     memb_number = request.form["memb_number"]
-    memb_type = request.form["memb_type"]
+    memb_type_id = request.form["memb_type"]
     address = request.form["address"]
     contact_number = request.form["contact_number"]
     active = request.form["active"]
 
-    member = Member(name, date_of_birth, memb_number, memb_type, address, contact_number, active, id)
+    memb_type = memb_type_repository.select(memb_type_id)
+
+    member = Member(image_url, name, date_of_birth, memb_number, memb_type, address, contact_number, active, id)
 
     member_repository.update(member)
     return redirect(f'/members/{id}')
